dffcdc544b8507cbf897b0536d386828bbd677cb
papadarfoor <papadarfoor@gmail.com>
2022-06-26 20:30:36 +0100

    search query

---
+++ functions/customer/buildings/searchBuildings.php
@@ -0,0 +1,22 @@
+<?php
+require '../../../database/mydbcon.php';
+
+$connect = dbconnect();
+
+$searchString = $_GET['search_string'];
+
+
+$sql = "SELECT * FROM buildings WHERE building_name LIKE '%$roomName%'";
+$result = mysqli_query($connect,$sql);
+
+$data = array();
+
+while($row = $result->fetch_assoc()){
+
+    $data[] = json_decode($row);
+
+}
+
+echo json_encode($data);
+
+?>
