b6c0e556fbe092b258e720063387c989af0b3d9a
papadarfoor <papadarfoor@gmail.com>
2022-06-26 02:40:29 +0100

    db setup

--- database/mydbcon.php
+++ database/mydbcon.php
@@ -3,10 +3,10 @@ session_start();
 
 function dbconnect(){
 //create constants for database variables
-$HOSTNAME = 'localhost';
-$USERNAME = 'root';
-$PASSWORD = '';
-$DBNAME = 'roomap'; 
+$HOSTNAME = 'eu-cdbr-west-02.cleardb.net';
+$USERNAME = 'b0c2770297daa0';
+$PASSWORD = '0928c090';
+$DBNAME = 'heroku_70a5e68d1e11b82'; 
 
 //make database connect
 $conn = new mysqli($HOSTNAME,$USERNAME,$PASSWORD,$DBNAME) or die("Connection failed");
