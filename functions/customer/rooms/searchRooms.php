d54f5e8f598544e83a87b3de2e6bfeb3bdfef56d
papadarfoor <papadarfoor@gmail.com>
2022-06-28 18:57:53 +0100

    search rooms

--- functions/customer/rooms/searchRooms.php
+++ functions/customer/rooms/searchRooms.php
@@ -4,9 +4,10 @@ require '../../../database/mydbcon.php';
 $connect = dbconnect();
 
 $searchString = $_GET['search_string'];
+$buildingId = $_GET['building_id'];
 
 
-$sql = "SELECT * FROM rooms WHERE room_name LIKE '%$searchString%'";
+$sql = "SELECT * FROM rooms WHERE building_id = $buildingId AND room_name LIKE '%$searchString%'";
 $result = mysqli_query($connect,$sql);
 
 $data = array();
