32adc285ee608960a0370bd111eec687acfe35d0
papadarfoor <papadarfoor@gmail.com>
2022-06-29 00:49:07 +0100

    Search Pathways

--- functions/customer/pathways/searchPathways.php
+++ functions/customer/pathways/searchPathways.php
@@ -5,6 +5,7 @@ require '../../../database/mydbcon.php';
 $connect = dbconnect();
 
 $roomId = $_GET['room_id'];
+$searchString = $_GET['search_string'];
 
 $sql = "SELECT * FROM pathways WHERE room_id = $roomId AND pathways LIKE '%$searchString%'";
 $result = mysqli_query($connect,$sql);
