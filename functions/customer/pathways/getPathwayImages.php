f5ac48473ce64c4110364a4c83bb722d1329007d
papadarfoor <papadarfoor@gmail.com>
2022-06-29 10:48:59 +0100

    pathway images

---
+++ functions/customer/pathways/getPathwayImages.php
@@ -0,0 +1,21 @@
+<?php
+require '../../../database/mydbcon.php';
+
+$connect = dbconnect();
+
+$roomId = $_GET['room_id'];
+$roomName = $_GET['room_name'];
+
+$sql = "SELECT * FROM pathways WHERE room_id = $roomId AND pathways LIKE '%$roomName%'";
+$result = mysqli_query($connect,$sql);
+
+
+while($row = $result->fetch_assoc()){
+
+    $data = json_decode($row['pathway_images']);
+
+}
+
+echo json_encode($data->room->pathway_direction);
+
+?>
